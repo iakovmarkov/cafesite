c03dd0f2e51a59a3dcb60a18eaa41d28602a903a
Iakov <markov@iakov.me>
2020-08-22 14:20:32 +0200

    feat: remove nuxt placeholders, configure basic app layout

---
+++ PALETTE.md
@@ -0,0 +1,10 @@
+# Palette for Dada Cafe
+
+Gray: #dad9e0
+Black: #2a204a
+Yellow: #ffd319
+Pink: #c233b4
+
+# Attribution
+
+<a target="_blank" href="https://icons8.com/icons/set/coffee">Coffee icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,57 +1,30 @@
 
 export default {
-  /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
   mode: 'universal',
-  /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
   target: 'server',
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Dada Cafe',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Unique cafe located right here, on the interwebz.' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+      { rel: "manifest", href: "/site.webmanifest" }
     ]
   },
-  /*
-  ** Global CSS
-  */
   css: [
   ],
-  /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
   plugins: [
   ],
-  /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
   components: true,
-  /*
-  ** Nuxt.js dev-modules
-  */
   buildModules: [
     '@nuxt/typescript-build',
   ],
-  /*
-  ** Nuxt.js modules
-  */
   modules: [
-    // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
     '@nuxtjs/bulma',
   ],
   /*
